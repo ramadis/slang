669174b983cc0f06ee7741eddccbf9df37b4ff67
Rama Olivera Fedi <rramiro.o@hotmail.com>
2016-07-04 23:24:22 -0300

    Added source code and readme

--- README.md
+++ README.md
@@ -1,2 +1,49 @@
 # slang
-Encoding/Decoding tool for Martin David's theoretical S language programs.
+
+Slang is a javascript library used to get the natural representation of a program from theoretical `L` (or `S`) language, from [Martin Davis' Computability, Complexity and Languages](https://www.amazon.com/Computability-Complexity-Languages-Second-Fundamentals/dp/0122063821).
+
+## Example
+A valid example of the `S` language is as follows
+```
+[A1] X1++;
+     X1--;
+     X1?A1;
+```
+To get the encoding for said program, store it into a variable and pass it to `program = Program.fromString(programString)`. Then, just call `program.getCode()` to obtain what you were looking for: `"2^703 3^22 5^1534 - 1"`.
+
+For further understanding, keep reading the theory.
+
+## Instructions
+To encode an instruction we use a (bijective) pairing function (Mapping from N^2 --> N) with the form `<x,y> = 2^x(2y+1)-1`.
+We divide the syntax in 3 disjoint groups and generate the code for an instruction using `<a,<b,c>>`.
+
+### Labels (encoded as `a`)
+Starting in 0, the labels are `[A1, B1, C1, D1, E1, A2, ...]`. They are optional in a given line.
+
+### Operations (encoded as `b`)
+Starting in 0, the possible operations inside the `S` languague are
+- `V` Does nothing
+- `V++` Increments the variable V in 1
+- `V--` Decrements the variable V in 1
+- `V?#L` If V is not zero, go to label L
+
+### Variables (encoded as `c`)
+Starting in 0, tha variables are `Y, X1, Z1, X2, Z2, X3, Z3, ...`. Being `Y` the outuput variable. the `X`s the input variables and the `Z`s the auxiliary variables.
+
+## Program encoding
+We encode programs using [Gödel numbering](https://en.wikipedia.org/wiki/G%C3%B6del_numbering). So a program is finally encoded as `[#I1, #I2, #I3, ...] - 1` being `#Ij` the code for the `jth` instruction, and `[]` the syntax using for the given Gödel number.
+
+## Documentation
+**Program Class**: Represents the program by holding an array of Instructions
+```
+fromString: returns a Program from a valid S language program string.
+getCode: returns the code for the Given program
+```
+**Instruction Class**: Represents an instruction, with its encoding representation.
+```
+fromString: returns an Instruction from a valid S language instruction string.
+fromCode: returns an Instruction from a valid instruction code.
+getCode: returns the code for an Instruction
+```
+## Notes
+Every encoding step can be proved, showing that exists a bijectivity between natural numbers and the `S` language programs. Further proving, that there are functions which can not be computed.
---
+++ source.js
@@ -0,0 +1,140 @@
+// Object holding error messages
+const ERROR = {
+  INVALID_LABEL: { code: 1, msg: 'THE LABEL PROVIDED FOR AN INSTRUCTION IS MALFORMED' },
+  INVALID_VAR: { code: 2, msg: 'THE VARIABLE PROVIDED FOR AN INSTRUCTION IS MALFORMED' },
+  INVALID_NUM: { code: 3, msg: 'THE NUMBER PROVIDED IS NOT ACTUALLY A NUMBER' }
+};
+
+
+// Represents an instruction, holding its representation in 3 fields.
+// See docs for further understanding.
+class Instruction {
+  constructor (a, b, c) {
+    this.a = a;
+    this.b = b;
+    this.c = c;
+  }
+
+  static fromCode (num) {
+    num = Number(num);
+    if (!num) throw new Error(ERROR.INVALID_NUM);
+
+    const fstCall = Pairing.inverse(num);
+    const sndCall = Pairing.inverse(fstCall.snd);
+    
+    const a = fstCall.fst;
+    const b = sndCall.fst;
+    const c = sndCall.snd;
+
+    return new Instruction(a, b, c);
+  }
+
+  static fromString (str) {
+    const instruction = {};
+    let [label, operation] = str.split(' ');
+    operation = operation ? operation : label;
+    if (!operation) return null;
+    if (operation === label) label = null;
+
+    instruction.a = InstructionParser.getLabelValue(label);
+    instruction.b = InstructionParser.getOperationValue(operation, label);
+    instruction.c = InstructionParser.getVariableValue(operation);
+
+    return new Instruction(instruction.a, instruction.b, instruction.c);
+  }
+
+  getCode () {
+    return Pairing.direct(this.a, Pairing.direct(this.b, this.c));
+  }
+};
+
+// Parses string into the values corresponding to an instruction.
+class InstructionParser {
+  static getLabelValue (label) {
+    if (!label) return 0;
+
+    const letter = label.match(/[a-e]/)[0];
+    const idx = Number(label.match(/[0-9]+/)[0]);
+    const pos = letter.charCodeAt(0) - 'a'.charCodeAt(0) + 1;
+
+    if (pos <= 0 || !idx) throw new Error(ERROR.INVALID_LABEL);
+    return ( pos + 5 ) * idx;
+  }
+
+  static getOperationValue (operation, label) {
+    // Invalid operations are considered null operations.
+    if (operation.includes('++')) return 1;
+    if (operation.includes('--')) return 2;
+    if (operation.includes('?')) return 2 + this.getLabelValue(operation.split('?')[1]);
+    return 0;
+  }
+
+  static getVariableValue (operation) {
+    let idx = operation.match(/[1-9]+/);
+
+    if (operation.includes('y')) return 0;
+    idx = idx ? Number(idx[0]) : null;
+    if (operation.includes('z') && idx) return 2 * idx;
+    if (operation.includes('x') && idx) return 2 * idx - 1;
+    debugger;
+    throw new Error(ERROR.INVALID_VAR);
+  }
+};
+
+// Represents a pairing function, which maps N^2 --> N
+class Pairing {
+  static _findFactor (num, fact) {
+    let aux = 0;
+    while (num % fact === 0 && num != 0) aux++;
+    return aux;
+  }
+
+  static direct (fst, snd) {
+    return Math.pow(2, fst) * (2 * snd + 1) - 1;
+  }
+
+  static inverse (num) {
+    const pair = { fst: 0, snd: 0 };
+
+    pair.fst = num / (this._findFactor((num + 1), 2) * 2);
+    pair.snd = (pair.fst - 1) / 2;
+
+    return pair;
+  }
+};
+
+// Represents the program. Its the main class.
+// Holds an array of instructions.
+class Program {
+  constructor (instructions) {
+    this.instructions = instructions;
+  }
+
+  static fromString (str) {
+    const instructionsStr = str.toLowerCase().split(';').map((instructionStr) => instructionStr.trim());
+    const instructions = instructionsStr.map((instructionStr) => Instruction.fromString(instructionStr))
+                                        .filter((instruction) => instruction);
+    return new Program(instructions);
+  }
+
+  getCode () {
+    let code = '';
+    const primes =  [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97, 101, 103,
+                    107, 109, 113, 127, 131, 137, 139, 149, 151, 157, 163, 167, 173, 179, 181, 191, 193, 197, 199, 211, 223,
+                    227, 229, 233, 239, 241, 251, 257, 263, 269, 271, 277, 281, 283, 293, 307, 311, 313, 317, 331, 337, 347,
+                    349, 353, 359, 367, 373, 379, 383, 389, 397, 401, 409, 419, 421, 431, 433, 439, 443, 449, 457, 461, 463,
+                    467, 479, 487, 491, 499, 503, 509, 521, 523, 541, 547, 557, 563, 569, 571, 577, 587, 593, 599, 601, 607,
+                    613, 617, 619, 631, 641, 643, 647, 653, 659, 661, 673, 677, 683, 691, 701, 709, 719, 727, 733, 739, 743,
+                    751, 757, 761, 769, 773, 787, 797, 809, 811, 821, 823, 827, 829, 839, 853, 857, 859, 863, 877, 881, 883,
+                    887, 907, 911, 919, 929, 937, 941, 947, 953, 967, 971, 977, 983, 991, 997, 1009, 1013, 1019, 1021, 1031,
+                    1033, 1039, 1049, 1051, 1061, 1063, 1069, 1087, 1091, 1093, 1097, 1103, 1109, 1117, 1123, 1129, 1151,
+                    1153, 1163, 1171, 1181, 1187, 1193, 1201, 1213, 1217, 1223, 1229, 1231, 1237, 1249, 1259, 1277, 1279,
+                    1283, 1289, 1291, 1297, 1301, 1303, 1307, 1319, 1321, 1327, 1361, 1367, 1373, 1381, 1399, 1409, 1423,
+                    1427, 1429, 1433, 1439, 1447, 1451, 1453, 1459, 1471, 1481, 1483, 1487, 1489, 1493, 1499, 1511, 1523,
+                    1531, 1543, 1549, 1553, 1559, 1567, 1571, 1579, 1583, 1597, 1601, 1607, 1609, 1613, 1619, 1621, 1627,
+                    1637, 1657, 1663, 1667, 1669, 1693, 1697, 1699, 1709, 1721, 1723, 1733, 1741, 1747, 1753, 1759, 1777,
+                    1783, 1787, 1789, 1801, 1811, 1823, 1831, 1847, 1861, 1867, 1871, 1873, 1877, 1879, 1889, 1901, 1907];
+    this.instructions.forEach((instruction, idx) => code += `${primes[idx]}^${instruction.getCode()} `);
+    return code  += "- 1";
+  }
+};
